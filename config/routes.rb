1957b5f1cfcc17f93ffaae4b86a1afbd0d1af581
Julia Gulia <lisagdale@sympatico.ca>
2011-10-06 10:13:49 -0400

    Fixed time bug

--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,7 @@ Haven::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+   root :to => "go#main"
 
   # See how all your routes lay out with "rake routes"
 
